0d5da0424baa1216154317fb9e671dd9e78fafb7
Jeff Wilson <MythicalMeerkat@users.noreply.github.com>
2018-09-06 16:47:20 -0600

    Initial File Commit
    Prototyping of project with header files and a default main function.

---
+++ Prog2/recordsList.h
@@ -0,0 +1,36 @@
+/*
+Jeffrey Wilson
+A04770255 - jaw291
+
+Programming Project 2
+*/
+
+// Defines the enty point for the header file dictating the student class.
+
+#include <string>
+#include "student.h"
+
+using namespace std;
+
+class recordsList
+{
+  private:
+    struct Node          // Each node represents a student
+    {
+        string Name;
+        string ID;
+        string studentAddress;
+        double GPA;
+
+        Node *next;      // points to next node in list
+    };
+
+    Node *head;           // the head pointer of the List
+
+  public:
+    recordsList();
+    ~recordsList();
+
+    void append(student*);
+    void displayRecords() const;
+};
---
+++ Prog2/student.h
@@ -0,0 +1,23 @@
+/*
+Jeffery Wilson
+A04770255 - jaw291
+
+Programming Project 2
+*/
+
+// Defines entry point for the student class header file.
+
+#include <string>
+
+class student{
+    private:
+        string name;
+        string ID;
+        string student_Address;
+        double GPA;
+
+    public:
+        student(string, string, string, GPA);
+
+
+};
